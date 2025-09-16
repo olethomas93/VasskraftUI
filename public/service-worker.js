aa3820e117e0049e9ff3478c501ae1549b78ec55
olethomas93 <norbye93@gmail.com>
2025-09-16 20:57:29 +0200

    e

--- public/service-worker.js
+++ public/service-worker.js
@@ -7,13 +7,6 @@ self.addEventListener('activate', (event) => {
   self.clients.claim()
 })
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request)
-    }),
-  )
-})
 
 self.addEventListener('push', (event) => {
   const data = event.data ? event.data.text() : 'New notification'

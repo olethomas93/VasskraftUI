764c398fecd58bdb7da5967d4fc17e644b62017f
olethomas93 <norbye93@gmail.com>
2025-09-16 20:24:18 +0200

    dw

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,16 +1,16 @@
 // Minimal service worker for offline support
-self.addEventListener('install', event => {
-  self.skipWaiting();
-});
+self.addEventListener('install', (event) => {
+  self.skipWaiting()
+})
 
-self.addEventListener('activate', event => {
-  self.clients.claim();
-});
+self.addEventListener('activate', (event) => {
+  self.clients.claim()
+})
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
-});
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request)
+    }),
+  )
+})
